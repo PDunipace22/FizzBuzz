445b146794ba7bf4b1fa6deecc23cab2077ab3d2
PDunipace22 <peterdunipace@gmail.com>
2018-01-30 11:04:37 -0500

    Add project files.

---
+++ FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void Get_Returns_A_Number()
+        {
+            
+        }
+    }
+}
